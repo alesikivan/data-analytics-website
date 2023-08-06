bd54c666fc671d5c89593bd87fc472fb9cd12221
alesikivan <obcofficiall@gmail.com>
2023-08-06 18:43:34 +0300

    redesign

--- src/shared/layouts/HomePageLayout.tsx
+++ src/shared/layouts/HomePageLayout.tsx
@@ -12,6 +12,8 @@ export const HomePageLayout = () => {
   return (
     <>
       <main className="home-page">
+        <h1 className='hidden'>Partner with MUNI's Data Analytic degree program and join future professionals on their journey to becoming Data Scientists. This unique partner program is designed to provide practitioners (including those already working in various IT industries) with the mathematical fundamentals, problem-solving skills, and mindset. At the same time, math-oriented students will gain practical skills and an applied vision in data analytics. Students will also develop data-driven thinking and problem-solving skills, and master the soft skills needed to successfully communicate data-driven quantitative insights to diverse professional and lay audiences.</h1>
+
         <Header />
   
         <Outlet />
--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -13,6 +13,8 @@ export const MainLayout = () => {
 
   return (
     <div className="application">
+      <h1 className='hidden'>Partner with MUNI's Data Analytic degree program and join future professionals on their journey to becoming Data Scientists. This unique partner program is designed to provide practitioners (including those already working in various IT industries) with the mathematical fundamentals, problem-solving skills, and mindset. At the same time, math-oriented students will gain practical skills and an applied vision in data analytics. Students will also develop data-driven thinking and problem-solving skills, and master the soft skills needed to successfully communicate data-driven quantitative insights to diverse professional and lay audiences.</h1>
+      
       <Header />
       
       <div className="app-container pb-6 mt-0 mb-2">
